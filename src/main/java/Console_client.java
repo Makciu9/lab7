5423f5a2123c5b99a9aa0b722b9a3bf34d00791a
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-12 23:57:59 +0300

    Scripted auto-commit on change (2020-02-12 23:57:59) by gitwatch.sh

--- src/main/java/Console_client.java
+++ src/main/java/Console_client.java
@@ -1,3 +1,4 @@
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
@@ -6,6 +7,8 @@ public class Console_client {
     private static ZMQ.Socket socket;
     public static void main(String[] args) {
         ZContext context = new ZContext();
+        socket = context.createSocket(SocketType.REQ);
         
+
     }
 }
