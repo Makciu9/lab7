67cca45207d58042674dd241360b56d6dd1a7321
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-14 21:36:02 +0300

    Scripted auto-commit on change (2020-02-14 21:36:02) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,9 +1,13 @@
+import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 
 public class Storage {
     private static ZContext context;
     public static void main(String[] args) {
         context = new ZContext();
+        ZMQ.Socket socket = context.createSocket(SocketType.DEALER);
+
+        
 
     }
 }
