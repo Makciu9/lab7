cfb9e0ccfcc469c398007d2efedd93aa95e870a0
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-14 21:32:24 +0300

    Scripted auto-commit on change (2020-02-14 21:32:24) by gitwatch.sh

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,9 +1,9 @@
 import org.zeromq.ZContext;
 
 public class Storage {
-
+    private static ZContext context;
     public static void main(String[] args) {
         context = new ZContext();
-        
+
     }
 }
