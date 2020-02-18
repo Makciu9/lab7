f0a69dbc76cc31b3642eebbd5451866d0d832537
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-02-18 15:45:43 +0300

    Scripted auto-commit on change (2020-02-18 15:45:43) by gitwatch.sh

--- src/main/java/Proxy.java
+++ src/main/java/Proxy.java
@@ -49,7 +49,9 @@ public class Proxy {
                     int start = Integer.parseInt(messageSplit[1]);
                     int end = Integer.parseInt(messageSplit[2]);
                     caches.add(new Cache(frame, id, System.currentTimeMillis(), start, end));
-                }else if (command.equals(
+                }else if (command.equals("TIMEOUT")){
+                    
+                }
 
 
 
