97eb631ff9dde4bc7fef65e0b61df3daa091c108
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-03-26 15:01:12 +0300

    Scripted auto-commit on change (2020-03-26 15:01:12) by gitwatch.sh

--- src/main/java/Proxy.java
+++ src/main/java/Proxy.java
@@ -10,17 +10,8 @@ public class Proxy {
     private static ArrayList<Cache> caches;
 
     public static void main(String[] args) {
-        context = new ZContext();
-        socketClient = context.createSocket(SocketType.ROUTER);
-        socketClient.bind("tcp://localhost:3585");
-
-        socketStorage = context.createSocket(SocketType.ROUTER);
-        socketStorage.bind("tcp://localhost:3586");
-        System.out.println("Proxy start");
-
-        poller = context.createPoller(2);
-        poller.register(socketClient, ZMQ.Poller.POLLIN);
-        poller.register(socketStorage, ZMQ.Poller.POLLIN);
+        initContextSockets();
+        initPoller();
 
         caches = new ArrayList<>();
         while (poller.poll(3000) != -1) {
@@ -29,7 +20,9 @@ public class Proxy {
                 String message = new String(recv.getLast().getData(), ZMQ.CHARSET);
                 String[] messageSplit = message.split(" ");
                 String command = messageSplit[0];
+
                 System.out.println(messageSplit[0] + " " + messageSplit[1]);
+
                 if (command.equals("GET")){
                     int key = Integer.parseInt(messageSplit[1]);
                     sendGET(key, recv);
@@ -60,6 +53,23 @@ public class Proxy {
         context.destroySocket(socketStorage);
         context.destroy();
     }
+
+    private static void initPoller() {
+        poller = context.createPoller(2);
+        poller.register(socketClient, ZMQ.Poller.POLLIN);
+        poller.register(socketStorage, ZMQ.Poller.POLLIN);
+    }
+
+    private static void initContextSockets() {
+        context = new ZContext();
+        socketClient = context.createSocket(SocketType.ROUTER);
+        socketClient.bind("tcp://localhost:3585");
+
+        socketStorage = context.createSocket(SocketType.ROUTER);
+        socketStorage.bind("tcp://localhost:3586");
+        System.out.println("Proxy start");
+    }
+
     private static void sendGET(int key, ZMsg recv) {
         boolean flag = false;
         for (Cache cache : caches) {
