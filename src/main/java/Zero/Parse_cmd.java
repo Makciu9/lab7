e38e522fa532698f9c61a87abbff83ff0f155d8f
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-05-07 16:10:49 +0300

    Scripted auto-commit on change (2020-05-07 16:10:49) by gitwatch.sh

--- src/main/java/Zero/Parse_cmd.java
+++ src/main/java/Zero/Parse_cmd.java
@@ -1,7 +1,15 @@
 package Zero;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Parse_cmd {
     enum Type{
         GET, PUT, INIT, TIMEOUT,
+    }
+
+    public Parse_cmd(String cmd){
+        tmp = new ArrayList<>();
+
     }
 }
