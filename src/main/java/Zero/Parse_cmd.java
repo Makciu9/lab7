efef67d2b270d40af9e706ca68acac479fa2dcce
Makc UZLOV <makcuzlov@MacBook-Pro-Makc.local>
2020-05-07 16:16:53 +0300

    Scripted auto-commit on change (2020-05-07 16:16:52) by gitwatch.sh

--- src/main/java/Zero/Parse_cmd.java
+++ src/main/java/Zero/Parse_cmd.java
@@ -8,7 +8,7 @@ public class Parse_cmd {
         GET, PUT, INIT, TIMEOUT,
     }
     private ArrayList<Integer> tmp;
-
+    private Type type;
     public Parse_cmd(String cmd){
         tmp = new ArrayList<>();
         parseCmd(cmd);
@@ -16,5 +16,16 @@ public class Parse_cmd {
     }
 
     private void parseCmd(String cmd) {
+        String[] split = cmd.split(" ");
+        if(split[0].equals("GET")){
+            this.type=Type.GET;
+            tmp.add(Integer.parseInt(split[1]));
+        }
+
+        if(split[0].equals("PUT")){
+            this.type=Type.PUT;
+            tmp.add(Integer.parseInt(split[1]));
+            tmp.add(Integer.parseInt(split[2]));
+        }
     }
 }
